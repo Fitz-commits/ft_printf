e0f282b2495435a7df94b5de1fef84d5dcb07c9b
Fitz-commit <cdelaby@student.s19.be>
2019-11-02 16:14:56 +0100

    minor change

---
+++ conv.c
@@ -0,0 +1,17 @@
+#include "ft_printf.h"
+#include <stdarg.h>
+
+int	conv_init(char data, va_list ap, int *i)
+//i is the progression on the initial string that need 
+//to be changed
+//
+//this function return a count of how many character it writes
+//
+{
+	int j;
+
+	j = 0;
+	if(data[1] == '%')
+		return (write(1, '%', 1);
+	
+}
--- ft_printf.c
+++ ft_printf.c
@@ -10,4 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+#include <stdio.h>
+#include <stdarg.h>
 
+int	ft_printf(const char *format, ...)
+{
+	va_list ap;
+	int count;
+	int i;
+
+	i = 0;
+	count = 0;
+	va_start(ap, format);
+	while(format[i])
+	{
+		if(format[i] == '%')
+			count += conv_init(&format[i], ap, &i)
+		else
+		{
+			count += write(1, format[i], 1)
+			i++;
+		}
+	}
+	return (count)
+}
